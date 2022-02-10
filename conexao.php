dfef30aa47c3bf887d71ae3c5ddf206a6dc4fd19
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-10 18:21:57 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $banco = "pokedex";
-    $conexao = new mysqli($servidor, $usuario, $senha, $banco);
-        if (mysqli_connect_errno()){
-            trigger_error((mysqli_connect_error()));
-            exit();
+    $servidor = "localhost";// servidor
+    $usuario = "root";// usuario
+    $senha = "";// senha
+    $banco = "pokedex";// banco de dados
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco);// conexao com o banco de dados
+    if(mysqli_connect_errno()){// se houver erro na conexao
+        trigger_error((mysqli_connect_error()));// dispara o erro
+            exit();// encerra a execucao
         }
-        mysqli_set_charset($conexao, 'utf8');
+        mysqli_set_charset($conexao, 'utf8');// seta o charset
 ?>
